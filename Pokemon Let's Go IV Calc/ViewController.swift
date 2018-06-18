191ca8f0c54cc9dff621f1e83c16c89ca356ff32
Joshua Oki <theokster@outlook.com>
2018-06-18 17:16:39 -0400

    0.1.0

--- Pokemon Let's Go IV Calc/ViewController.swift
+++ Pokemon Let's Go IV Calc/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var pokemonPicker: UIPickerView!
+    var pokemonArray: [String] = Array()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pokemonArray.append("Bulbasaur")
+        pokemonArray.append("Ivysaur")
+        pokemonArray.append("Venusaur")
+
+        // assign our class for delegates and datasource methods for the picker
+        pokemonPicker.delegate = self
+        pokemonPicker.dataSource = self
+
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pokemonArray.count
+    }
+    
+    //MARK:- UIPickerViewDelegates methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pokemonArray[row]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
